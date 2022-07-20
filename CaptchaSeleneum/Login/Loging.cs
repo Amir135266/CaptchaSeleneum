a18a1464b8bef034d24203a3363eac7d903a1cd0
A.Edalati <A.Edalati@PC-KAMALI>
2022-07-20 15:19:30 +0430

    Add project files.

---
+++ CaptchaSeleneum/Login/Loging.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace CaptchaSeleneum.Login
+{
+    [TestFixture]
+    [Parallelizable]
+    public class Loging
+    {
+        IWebDriver webDriver = null;
+        IWebElement webElement = null;
+        readonly int milliSeconds = 2000;
+
+        [SetUp]
+        public void Openbrwser()
+        {
+            webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("https://eg-task.buzz/login.php");
+        }
+
+        [Test]
+        public void SendAndClick()
+        {
+            webElement = webDriver.FindElement(By.XPath("//*[@id='email']"));
+            webElement.SendKeys("amirhossineedalat2020@gmail.com");
+
+            webElement = webDriver.FindElement(By.XPath("//*[@id='password']"));
+            webElement.SendKeys("amir1352");
+
+            webElement = webDriver.FindElement(By.XPath("//*[@id='login-form']/div[4]/button"));
+            webElement.Click();
+            Thread.Sleep(milliSeconds);
+
+            webDriver.FindElement(By.LinkText("Challenge typing speed")).Click();
+            Thread.Sleep(milliSeconds);
+
+            var done = webDriver.FindElement(By.Id("btnSubmitId"));
+            done.Click();
+            Thread.Sleep(milliSeconds);
+
+            String framename = webDriver.FindElement(By.XPath("//*[@id='ccc']")).GetAttribute("src");
+            webDriver.SwitchTo().Frame(framename);
+            //webDriver.FindElement(By.XPath("//*[@id='ccc']")).Click();
+            webElement.SendKeys(framename);
+
+            webElement = webDriver.FindElement(By.TagName("input"));
+            String title = webElement.GetAttribute("src");
+        }
+        
+        [TearDown]
+        public void TearDown()
+        {
+            TestContext.WriteLine("Closed!");
+        }
+    }
+}
